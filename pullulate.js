f9b43a841075fe2e4c1329c6692b340f907656ce
Jay Sanders <mindtonic@gmail.com>
2012-07-24 10:53:11 -0400

    Initial Pullulate Files

---
+++ pullulate.js
@@ -0,0 +1,30 @@
+(function( $ ) {
+  $.fn.pullulate = function(options) {
+
+    // Default Settings
+    var settings = $.extend( {
+      'hidden_div' : 'pullulate-hiddendiv',
+      'textarea_padding' : 20
+    }, options);
+
+    function swell(element) {
+    	textarea = $(element);
+    	hiddenDiv = textarea.siblings('.'+settings['hidden_div']);
+	    hiddenDiv.html(textarea.val().replace(/\n/g, '<br>'));
+
+	    textarea.css('height', hiddenDiv.height() + settings['textarea_padding']);
+    }
+
+    return this.each(function() {
+    	textarea = $(this);
+    
+			textarea.addClass('pullulate');
+	    var hiddenDiv = textarea.after($('<div>').attr({ 'class' : settings['hidden_div'] }));
+	    swell(this);
+
+			textarea.bind('keyup blur change', function() {
+				swell(this);
+			});
+    });
+  };
+})( jQuery );
